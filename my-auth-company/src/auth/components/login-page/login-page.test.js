6700b08d3123808f466b7ff5a168dd11c20f4ead
david-borja <davidborjamartinez@gmail.com>
2025-05-27 00:32:07 +0200

    test(): focus and change with valid email after blur with invalid email

--- my-auth-company/src/auth/components/login-page/login-page.test.js
+++ my-auth-company/src/auth/components/login-page/login-page.test.js
@@ -41,8 +41,8 @@ describe('when the user fills the fields and clicks the submit button', () => {
   })
 })
 
-describe('when the user fills and blur the email input with invalid email', () => {
-  it('must display a validation message "The email is invalid. Example: john.doe@mail.com"', () => {
+describe('when the user fills and blur the email input with invalid email, and then focus and change with valid value', () => {
+  it('must not display a validation message', () => {
     const emailInput = screen.getByLabelText(/email/i)
 
     // change and blur email input
@@ -51,10 +51,18 @@ describe('when the user fills and blur the email input with invalid email', () =
     })
     fireEvent.blur(emailInput)
 
-    // expect
     expect(
       screen.getByText(/the email is invalid. Example: john.doe@mail.com/i),
     ).toBeInTheDocument()
+
+    fireEvent.change(emailInput, {
+      target: {value: 'john.doe@email.com'},
+    })
+    fireEvent.blur(emailInput)
+
+    expect(
+      screen.queryByText(/the email is invalid. Example: john.doe@mail.com/i),
+    ).not.toBeInTheDocument()
   })
 })
 
